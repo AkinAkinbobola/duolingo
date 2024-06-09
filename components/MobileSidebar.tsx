c83045f17b1e0aed0941baab1437c7e99e61c637
Akin Akinbobola <akinbobola71@gmail.com>
2024-06-09 22:23:48 +0100

    Add main app layout

---
+++ components/MobileSidebar.tsx
@@ -0,0 +1,18 @@
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { MenuIcon } from "lucide-react";
+import Sidebar from "@/components/Sidebar";
+
+const MobileSidebar = () => {
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <MenuIcon className={"text-white"} />
+      </SheetTrigger>
+      <SheetContent className={"p-0 z-[100]"} side={"left"}>
+        <Sidebar />
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default MobileSidebar;

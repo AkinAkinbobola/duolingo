487d93f5a4d550bb9ea532b2ba9289303a32bf21
Akin Akinbobola <akinbobola71@gmail.com>
2024-06-10 10:01:39 +0100

    Add feed header

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,8 +1,15 @@
+import StickyWrapper from "@/components/StickyWrapper";
+import FeedWrapper from "@/components/FeedWrapper";
+import Header from "@/app/(main)/learn/Header";
+
 const LearnPage = () => {
     return (
-        <div>
-            LearnPage
-        </div>
+      <div className={"flex gap-[48px] px-6"}>
+        <FeedWrapper>
+          <Header title={"Japanese"} />
+        </FeedWrapper>
+        <StickyWrapper>My sticker wrapper</StickyWrapper>
+      </div>
     );
 };
 
